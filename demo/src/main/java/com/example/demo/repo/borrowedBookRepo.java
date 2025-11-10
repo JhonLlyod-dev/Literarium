6ed24cb7147143035a8e369230fe046a81643a29
nimbl <nimblebeans@gmail.com>
2025-11-10 16:57:58 +0800

    commit ko bay

--- demo/src/main/java/com/example/demo/repo/borrowedBookRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.BorrowedBook;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface borrowedBookRepo extends JpaRepository<BorrowedBook, Long> {
-}
